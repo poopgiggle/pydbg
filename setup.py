e1e29a3b70098f76ccfb6352048736551df6175f
poopgiggle <poopgiggle@gmail.com>
2016-08-02 13:52:49 -0500

    initial try for pydbg with pip and python2.7

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name="pydbg",
+	version="1.0",
+	author="pedram amini",
+	description="An experimental attempt to update pydbg to Python 2.7. Some stuff probably doesn't work.",
+	packages=["pydbg"],
+	)
